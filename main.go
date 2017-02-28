095308d05c74c571e417ce8f42e07d060ffad5a3
guozw <guozhiwei@inovance.cn>
2017-02-28 11:03:08 +0800

    1...

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+* @Author: guozhiwei
+* @Date:   2017-02-28 11:01:33
+* @Last Modified by:   guozhiwei
+* @Last Modified time: 2017-02-28 11:01:51
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world.")
+}
