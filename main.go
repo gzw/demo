759d5a6155da6df1b9dcdef56eb444d9ceefaeab
guozw <guozhiwei@inovance.cn>
2017-02-28 14:52:00 +0800

    1. just for testing.

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 /*
 * @Author: guozhiwei
 * @Date:   2017-02-28 11:01:33
-* @Last Modified by:   guozhiwei
-* @Last Modified time: 2017-02-28 11:01:51
+* @Last Modified by:   guozw
+* @Last Modified time: 2017-02-28 14:51:07
  */
 
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello world.")
+	http.HandleFunc("/", index)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("helloworld"))
 }
